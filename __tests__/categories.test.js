2643e5e5f04f90fe934dd6d7600142c082dfd86a
leeroywking <leeroywking@gmail.com>
2019-07-07 12:36:07 -0700

    linting is good

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -50,32 +50,32 @@ describe('Categories Model', () => {
       .then(record => {
         return categories.delete(record._id)
           .then(category => {
-            expect(categories.get(record._id).name).toBeFalsy()
-          })
-      })
-  })
+            expect(categories.get(record._id).name).toBeFalsy();
+          });
+      });
+  });
 
   it('can update a category', () => {
-    let obj = { name: 'Test Category', zoo:true };
-     categories.create(obj)
+    let obj = { name: 'Test Category', zoo: true };
+    categories.create(obj)
       .then(record => {
         categories.update(record.id, { name: 'New Test Category', id: 55 })
           .then(category => {
             categories.get(55)
               .then(zz => {
-                expect(zz.name).toEqual('New Test Category')
+                expect(zz.name).toEqual('New Test Category');
               })
-              .catch(err => console.err)
-          })
-      })
-  })
+              .catch(err => console.err);
+          });
+      });
+  });
 
   it('rejects bad type checks', () => {
-    let obj = { name: 555};
+    let obj = { name: 555 };
     categories.create(obj)
       .then(record => {
-        expect(record.id).toBeUndefined()
+        expect(record.id).toBeUndefined();
       })
-      .catch(err => console.error)
-  })
-})
+      .catch(err => console.error);
+  });
+});
